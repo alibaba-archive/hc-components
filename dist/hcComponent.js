125cefe563c30b286dc792878753a23a02df43c9
雨异 <yuyi.zsj@taobao.com>
2019-03-15 19:41:27 +0800

    __webpack_public_path__

--- dist/hcComponent.js
+++ dist/hcComponent.js
@@ -4830,12 +4830,12 @@ function staticPublicPath(name, standalone) {
 
   /* eslint-disable no-undef,camelcase */
   if (!standalone && window.CONFIG && window.CONFIG.staticPath) {
-    __webpack_require__.p = window.__static_public_path__ = window.CONFIG.staticPath + '/';
+    window.__webpack_public_path__ = window.__static_public_path__ = window.CONFIG.staticPath + '/';
   } else {
     if (document.domain.slice(-4) === '.com' && (!window.CONFIG || window.CONFIG.ENV === 'online')) {
-      __webpack_require__.p = window.__static_public_path__ = '//g.alicdn.com/' + workspace + name + '/0.0.1/';
+      window.__webpack_public_path__ = window.__static_public_path__ = '//g.alicdn.com/' + workspace + name + '/0.0.1/';
     } else if (document.domain.indexOf('local') !== 0) {
-      __webpack_require__.p = window.__static_public_path__ = '//g-assets.daily.taobao.net/' + workspace + name + '/0.0.1/';
+      window.__webpack_public_path__ = window.__static_public_path__ = '//g-assets.daily.taobao.net/' + workspace + name + '/0.0.1/';
     }
   }
 }
